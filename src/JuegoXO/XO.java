e320776e177bd414c52f30ed9105d6e922188da1
Leothomasv <leothomasv@unitec.edu>
2018-11-30 22:33:57 -0600

    update

---
+++ src/JuegoXO/XO.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JuegoXO;
+
+import static JuegoXO.Juego.muestraContenido;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Leonel Thomas
+ */
+public class XO {
+    
+    public static void main(String[] args) {
+        Juego JG=new Juego();
+        Scanner leer=new Scanner(System.in);
+        
+        int op;
+        try{
+         do{   
+        System.out.println("JUEGO DE \"x\" Y \"0\"" );
+        System.out.println("1. Jugar"
+                + "\n2 .ver partidas"
+                + "\n3. Cambiar letra del Jugador"
+                + "\n4. Cambiar Nombre del jugador "
+        +"\n5. Salir");
+        op=leer.nextInt();
+        switch(op){
+            case 1:
+               JG.autoLlenado();
+               JG.imprimirTablero();
+               JG.jugar();
+                break;
+
+            case 2:
+                System.out.println("Lista de partidas: ");
+                String m="Archivo.txt";
+                muestraContenido(m);
+                break;
+
+            case 3:
+               JG.asignarSimbolo();
+                break;
+
+            case 4:
+                JG.asignarNombre();
+                break;
+        }
+         }while(op !=5);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+        
+    
+
+}
